68dd5f69053772551d30de86b2372d31111642b6
75-akimas <akms2679@gmail.com>
2020-04-22 10:14:19 +0900

    Add fmt::Display to token and ast

--- src/ast.rs
+++ src/ast.rs
@@ -4,8 +4,20 @@ use crate::lexer::Lexer;
 #[derive(Clone, Debug, PartialEq)]
 pub enum Statement {
     LetStatement { token: Token, name: Expression, value: Expression },
-    ReturnStatement { token: Token, value: Expression }
+    ReturnStatement { token: Token, value: Expression },
+    ExpressionStatement { token: Token, expr: Expression }
 }
+
+impl std::fmt::Display for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", match self {
+           Statement::LetStatement{ token, name, value } => format!("{} {} = {};", token, if let Expression::Identifier(x) = name { x } else { &Token::Illegal }, Token::Illegal),
+           Statement::ReturnStatement{ token, value } => format!("{} {};", token, Token::Illegal),
+           _ => "".to_string()
+        })
+    }
+}
+
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expression {
@@ -13,3 +25,27 @@ pub enum Expression {
 }
 
 pub type Program = Vec<Statement>;
+
+#[cfg(test)]
+mod tests {
+    use crate::token::Token;
+    use crate::lexer::Lexer;
+    use crate::ast;
+    
+    #[test]
+    fn test_string() {
+        let program: ast::Program = vec![
+            ast::Statement::LetStatement{ token: Token::Let, name: ast::Expression::Identifier(Token::Ident("myVar".to_string())), value: ast::Expression::Identifier(Token::Illegal) },
+            ast::Statement::ReturnStatement{ token: Token::Return, value: ast::Expression::Identifier(Token::Illegal) },
+        ];
+
+        let tests = vec![
+            "let myVar = ILLEGAL;",
+            "return ILLEGAL;"
+        ];
+
+        for (i, tt) in tests.iter().enumerate() {
+            assert_eq!(**tt, format!("{}", program[i]));
+        }
+    }
+}
--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Token {
     Illegal,
     Eof,
@@ -36,43 +36,44 @@ pub enum Token {
     NotEqual
 }
 
-impl Token {
-    pub fn token_literal(token: Token) -> String {
-        match token {
-            Token::Illegal => "ILLEGAL".to_string(),
-            Token::Eof => "EOF".to_string(),
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", 
+            match self {
+                Token::Illegal => "ILLEGAL".to_string(),
+                Token::Eof => "EOF".to_string(),
 
-            Token::Ident(x) => x.clone(),
-            Token::Int(x) => x.to_string(),
+                Token::Ident(x) => x.clone(),
+                Token::Int(x) => x.to_string(),
 
-            Token::Assign => "=".to_string(),
-            Token::Plus => "+".to_string(),
-            Token::Minus => "-".to_string(),
-            Token::Bang => "!".to_string(),
-            Token::Asterisk => "*".to_string(),
-            Token::Slash => "/".to_string(),
+                Token::Assign => "=".to_string(),
+                Token::Plus => "+".to_string(),
+                Token::Minus => "-".to_string(),
+                Token::Bang => "!".to_string(),
+                Token::Asterisk => "*".to_string(),
+                Token::Slash => "/".to_string(),
 
-            Token::Lt => "<".to_string(),
-            Token::Gt => ">".to_string(),
+                Token::Lt => "<".to_string(),
+                Token::Gt => ">".to_string(),
 
-            Token::Comma => ".to_string(),".to_string(),
-            Token::Semicolon => ";".to_string(),
+                Token::Comma => ".to_string(),".to_string(),
+                Token::Semicolon => ";".to_string(),
 
-            Token::Lparen => "(".to_string(),
-            Token::Rparen => ")".to_string(),
-            Token::Lbrace => "{".to_string(),
-            Token::Rbrace => "}".to_string(),
+                Token::Lparen => "(".to_string(),
+                Token::Rparen => ")".to_string(),
+                Token::Lbrace => "{".to_string(),
+                Token::Rbrace => "}".to_string(),
 
-            Token::Function => "FUNCTION".to_string(),
-            Token::Let => "LET".to_string(),
-            Token::If => "IF".to_string(),
-            Token::Return => "RETURN".to_string(),
-            Token::Else => "ELSE".to_string(),
-            Token::True => "TRUE".to_string(),
-            Token::False => "FALSE".to_string(),
+                Token::Function => "fn".to_string(),
+                Token::Let => "let".to_string(),
+                Token::If => "if".to_string(),
+                Token::Return => "return".to_string(),
+                Token::Else => "else".to_string(),
+                Token::True => "true".to_string(),
+                Token::False => "false".to_string(),
 
-            Token::Equal => "==".to_string(),
-            Token::NotEqual => "!=".to_string()
-        }
+                Token::Equal => "==".to_string(),
+                Token::NotEqual => "!=".to_string()
+            })
     }
 }
